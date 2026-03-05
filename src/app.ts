df23d71338fb3e540604af19a9833b04f2ab737d
Vivek Singh <notanetizen@gmail.com>
2026-03-05 22:22:41 +0530

    add configuration

---
+++ src/app.ts
@@ -0,0 +1,25 @@
+import express from "express";
+import cors from "cors";
+
+const app = express();
+
+//basic config
+app.use(express.json({ limit: "16kb" }));
+app.use(express.urlencoded({ extended: true, limit: "16kb"}));
+app.use(express.static("public"));
+
+//cors config
+app.use(cors({
+    origin: process.env.CORS_ORIGIN?.split(",") || "http://localhost:5173",
+    credentials: true,
+    methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+}));
+
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+});
+
+
+export default app;
